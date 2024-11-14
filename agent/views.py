752434469397a6595175c051b158424f344449a5
Dilabar Hussain <dilbarh2@gmail.com>
2024-11-15 00:13:48 +0530

    agent update fix

--- agent/views.py
+++ agent/views.py
@@ -163,8 +163,8 @@ def twilio_service_detail_view(request):
 @login_required
 def create_or_update_agent(request, id=None):
     # If agent_id is provided, try to fetch the agent for editing
-    if id:
-        agent = get_object_or_404(Agent, id=id, user=request.user)
+    if id is not None:
+        agent = Agent.objects.filter(id=id, user=request.user).first()
     else:
         agent = None  # No existing agent, creating a new one
 
