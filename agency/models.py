d3f2775711c626cbffe9f8f34959e768f203f927
Dilabar Hussain <dilbarh2@gmail.com>
2025-03-12 08:54:10 +0530

    updated codes

--- agency/models.py
+++ agency/models.py
@@ -199,52 +199,11 @@ class AccountSwitchLog(models.Model):
 
     def __str__(self):
         return f"{self.agency.username} switched to {self.switched_to.username} at {self.switch_time}"
-# # Define the Agency model
-# class Agency(models.Model):
-#     name = models.CharField(max_length=100)
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)  # The agency is linked to a user (administrator)
-
-#     def __str__(self):
-#         return self.name
-
-# # Define the Client/Subaccount model
-# class Subaccount(models.Model):
-#     agency = models.ForeignKey(Agency, on_delete=models.CASCADE, related_name="subaccounts")
-#     name = models.CharField(max_length=100)
-#     email = models.EmailField()
+# Company Model
+# class Company(models.Model):
+#     name = models.CharField(max_length=255)
+#     agency = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={'user_type': 'agency'})
 #     created_at = models.DateTimeField(auto_now_add=True)
-
-#         # General Info Fields
-#     address = models.CharField(max_length=255, blank=True, null=True)
-#     city = models.CharField(max_length=100, blank=True, null=True)
-#     phone_number = models.CharField(max_length=15, blank=True, null=True)  # Assuming phone number in E.164 format
-#     # Timezone validation using choices
-#     TIMEZONE_CHOICES = [(tz, tz) for tz in pytz.all_timezones]
-#     timezone = models.CharField(
-#         max_length=100, 
-#         choices=TIMEZONE_CHOICES, 
-#         blank=True, 
-#         null=True
-#     )
-
-
+    
 #     def __str__(self):
 #         return self.name
-
-# # Define Role and Permissions (could use Django's built-in Groups and Permissions, but let's define custom ones for flexibility)
-# class Role(models.Model):
-#     name = models.CharField(max_length=50)
-#     permissions = models.TextField()  # JSON or text-based permissions (e.g., "view_data", "edit_data")
-
-#     def __str__(self):
-#         return self.name
-
-# # Define the Configuration model (CRM, Automations, etc.)
-# class Configuration(models.Model):
-#     subaccount = models.OneToOneField(Subaccount, on_delete=models.CASCADE, related_name="configuration")
-#     crm_enabled = models.BooleanField(default=True)
-#     automation_enabled = models.BooleanField(default=True)
-#     user_limit = models.PositiveBigIntegerField(default=5)
-
-#     def __str__(self):
-#         return f"Configuration for {self.subaccount.name}"
